95f792d4db9ef9e851e465413a3b1aeb616e719e
EmzB <emalibella45@gmail.com>
2022-03-11 05:32:04 +0300

    change 5 and 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Start of program
+ *
+ * Return: Return value is zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

607f2871b017e0c1348243575fd3f11cc876cfe0
EmzB <emalibella45@gmail.com>
2022-03-11 05:45:01 +0300

    all

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Start of program
+ *
+ * Return - Value is zero
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,22 +3,21 @@
 /**
  * main - Start of the program
  *
- * Return: Return value is zero 
+ * Return: Return value is zero
  */
 
 int main(void)
 {
-	char acharacter;
-	int ainteger;
-	long along;
-	long long alonglong;
-	float afloat;
+        char acharacter;
+        int ainteger;
+        long along;
+        long long alonglong;
+        float afloat;
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
-	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
-	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
-
+        printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
+        printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
+        printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+        printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+        printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
 	return (0);
 }

b1dc06826241d831a44e4bed88be402aa15bdb8a
EmzB <emalibella45@gmail.com>
2022-03-18 00:18:15 +0300

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * prints the alphabetx10 - print alphabet
+ *
+ * Return: Success 0.
+ */
+void print_alphabet_x10(void)
+{
+int alphabet;
+int count;
+
+
+count = 0;
+while (count < 10)
+{
+for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+
+count++;
+_putchar('\n');
+}
+
+
+}

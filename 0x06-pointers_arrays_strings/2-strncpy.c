7048f7066a54e3191cf44e5cdd888526248dcf2f
EmzB <emalibella45@gmail.com>
2022-03-25 03:53:33 +0300

    compare strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strncpy - Copies a string up to @n bytes
+ *
+ * @dest: src string is copied to it
+ *
+ * @src: string to be copied
+ *
+ * @n: Number of bytes of src that should be copied to dest
+ *
+ * Return: Pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		*(dest + i) = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}

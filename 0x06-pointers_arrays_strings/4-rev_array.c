faa2f6da6ed34f9a1768bd9d530b4bd328015770
EmzB <emalibella45@gmail.com>
2022-03-25 04:08:38 +0300

    string manaipulation

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,12 +1,9 @@
 #include "main.h"
 /**
- * reverse_array - Reverses an array of integers
- *
- * @a: An array 
- *
- * @n: number of values to swap
- *
- * Return: No return
+ * reverse_array - reverses the content of an array of integers
+ * @a: an array of integers
+ * @n: number of elements of the array
+ * Return: returns void
  */
 void reverse_array(int *a, int n)
 {

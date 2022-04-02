7bbad0a930ffa459415eda29f37b0fff928f5172
EmzB <emalibella45@gmail.com>
2022-04-02 05:30:38 +0300

    static libraries

--- 0x09-static_libraries/_puts.c
+++ 0x09-static_libraries/_puts.c
@@ -1,13 +1,7 @@
-#include <unistd.h>
+#include "main.h"
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+int main(void)
 {
-return (write(1, &c, 1));
+  _puts("\"They could never beat my outfit\"\n\t- Riri Fenty");
+  return (0);
 }
